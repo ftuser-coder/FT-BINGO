9dc09f05db22bfcf74ec211af6a65a2d83f1b630
ftuser-coder <iifetene@gmail.com>
2026-09-20 23:27:26 +0300

    Change Telegram bot token
    Updated the Telegram bot token for authentication.

--- functions/telegram.js
+++ functions/telegram.js
@@ -1,5 +1,5 @@
 const https = require("https");
-const TELEGRAM_BOT_TOKEN = "8618935818:AAFTP-2-s-5vlamwcYPVDB4STTR19VdHiVg";
+const TELEGRAM_BOT_TOKEN = "8618935818:AAGE8rEAi8WG9xYczpueEROz4IFUj8ukvmY";
 
 function callTelegramApi(method, payload) {
   return new Promise((resolve, reject) => {
