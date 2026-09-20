340c1aef0306e221ce57508b8466957604ddfacf
ftuser-coder <iifetene@gmail.com>
2026-09-20 23:48:30 +0300

    Update Telegram bot token

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ const { getCard, hasBingo } = require("./cartela");
 const { sendMessage, answerCallbackQuery, editMessageText } = require("./telegram");
 
 // ============ FT BINGO CONFIGURATION ============
-const TELEGRAM_BOT_TOKEN = "8618935818:AAGE8rEAi8WG9xYczpueEROz4IFUj8ukvmY";
+const TELEGRAM_BOT_TOKEN = "8618935818:AAHRiAfYV_yb-QDzfxjV2_XTURvgknAgi_c";
 const ADMIN_TELEGRAM_ID = 6502378631; 
 const DEFAULT_TELEBIRR_NUMBER = "0969437501"; 
 const FIRST_DEPOSIT_BONUS = 10;
