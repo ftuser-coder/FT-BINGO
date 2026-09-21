2ed7c5936eff5b150cff1ed73a7759bafe55e7a8
ftuser-coder <iifetene@gmail.com>
2026-09-21 20:44:10 +0300

    Change Telegram bot token
    Updated the Telegram bot token for the application.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { getCard, hasBingo } = require("./functions/cartela");
 const { sendMessage, answerCallbackQuery, editMessageText } = require("./functions/telegram");
 
 // ============ FT BINGO CONFIGURATION ============
-const TELEGRAM_BOT_TOKEN = "8618935818:AAHRiAfYV_yb-QDzfxjV2_XTURvgknAgi_c";
+const TELEGRAM_BOT_TOKEN = "8313850405:AAHZis31QfYGr3RF6_YfkMXn4ZEVQNmeA44";
 const ADMIN_TELEGRAM_ID = 6502378631; 
 const DEFAULT_TELEBIRR_NUMBER = "0969437501"; 
 const FIRST_DEPOSIT_BONUS = 10;
