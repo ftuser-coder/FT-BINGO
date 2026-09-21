09c10e08db424b7e244466fbd9addeeb573e0565
ftuser-coder <iifetene@gmail.com>
2026-09-21 22:27:50 +0300

    Fix formatting of TELEGRAM_BOT_TOKEN declaration

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const { getCard, hasBingo } = require("./functions/cartela");
 const { sendMessage, answerCallbackQuery, editMessageText } = require("./functions/telegram");
 
 // ============ FT BINGO CONFIGURATION ============
-const TELEGRAM_BOT_TOKEN = "8313850405:AAFKbGf0lrXfXePWyESTkOYdMwyjODjZ7pY
-";
+const TELEGRAM_BOT_TOKEN = "8313850405:AAFKbGf0lrXfXePWyESTkOYdMwyjODjZ7pY";
 const ADMIN_TELEGRAM_ID = 6502378631; 
 const DEFAULT_TELEBIRR_NUMBER = "0969437501"; 
 const FIRST_DEPOSIT_BONUS = 10;
